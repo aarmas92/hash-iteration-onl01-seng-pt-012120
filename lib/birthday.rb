85c13c9ea6b840683aaa050916647e81427e0ecb
Alvaro Armas <aarmas92@outlook.com>
2020-02-02 20:12:46 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -5,7 +5,9 @@
 # }
 
 def happy_birthday(birthday_kids)
-"Happy Birthday #{kids_name}! You are now #{age} years old!"
+  birthday_kids.each do |kids_name, age|
+    puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
+  end
 end
 
 
